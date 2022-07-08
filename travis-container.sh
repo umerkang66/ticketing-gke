a002bb3a5e8131ff812c2590fe0ec9b4ef1eba15
umerkang66 <umerkang66@gmail.com>
2022-07-08 12:11:55 +0500

    added encrypted service-account file

---
+++ travis-container.sh
@@ -0,0 +1,11 @@
+# we have added volume (bind mount) because we want to add service-account.json file inside of container
+docker run -it -v $(pwd):/app ruby:2.4 sh
+# docker run -it -v ${pwd}:/app ruby:2.4 sh --> FOR WINDOWS
+
+# after we are inside the container shell install travis-ci cli
+gem install travis
+
+# login with github tokens "repo", "user:email", "read:org"
+travis login --github-token <github_token> --com
+
+travis encrypt-file service-account.json -r <github_username>/<repo_name> --com
